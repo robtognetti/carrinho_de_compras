0b0dc868b584164b65184847fadeef8f4572b5a1
BradockBr <robtognetti@gmail.com>
2022-08-15 21:14:24 -0300

    fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (item) => {
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
+    const resposta = await fetch(url);
+    const data = await resposta.json();
+    return data;
+  } catch (error) {
+    return new Error('You must provide an url');
+  }
 };
 
 if (typeof module !== 'undefined') {
